d8f75451cc398c79cccf3e786f371b2cd1b92832
levanmanh <lvmanh25493@gmail.com>
2020-06-03 14:34:23 +0700

    Change name file 1

--- app/controllers/users/documents_controller.rb
+++ app/controllers/users/documents_controller.rb
@@ -52,7 +52,7 @@ module Users
         doc.bookmarks["company"].insert_text_after(result[5])
         doc.bookmarks["chief"].insert_text_after(result[11])
         doc.bookmarks["price"].insert_text_after(result[9])
-        source = "#{Rails.root}/public/documents/広報活動委託契約書[#{result[6]]}.docx"
+        source = "#{Rails.root}/public/documents/広報活動委託契約書[#{result[6]}].docx"
         doc.save(source)
         file_name = "#{result[5]}_#{today_string}"
         file_metadata = {
